b62a2db97e70a18bc7b1fb36b1c83e17db4d1595
matteovecchio04 <matteovex@gmail.com>
2025-12-25 16:00:08 +0100

    add for cycle to create an array of numbers from 1 to 90

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import { useState } from 'react'
 
 export default function Tombola() {
 
+  // Create the array with a for cycle
+  const createNumbers = () => {
+    const numbers = []
+    for (let i = 1; i <= 90; i++) {
+      numbers.push(i)
+    }
+    return numbers
+  }
+
   return (
     <>
       <h1>hello</h1>
